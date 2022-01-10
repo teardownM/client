8f9b8ebd04d9a8f143e7f9fe49ea8d4cbfc61f6e
vulcan-dev <vitexuhd@gmail.com>
2022-01-10 13:32:26 +0000

    Updated Discord

--- SledgeMP/Discord.cs
+++ SledgeMP/Discord.cs
@@ -1,4 +1,5 @@
 using DiscordRPC;
+using SledgeLib;
 
 public static class Discord {
     public enum EDiscordState : uint {
@@ -8,33 +9,66 @@ public static class Discord {
     }
 
     private static DiscordRpcClient? Client;
+    public static Party Party = new Party();
 
     public static Dictionary<EDiscordState, RichPresence> Presences = new Dictionary<EDiscordState, RichPresence>();
 
     public static bool Initialize() {
+        Random rand = new Random();
+
         Presences.Add(EDiscordState.MainMenu, new RichPresence {
             State = "In Main-Menu",
             Timestamps = new Timestamps() {
-            Start = DateTime.UtcNow
-        }});
+                Start = DateTime.UtcNow
+            },
+            Assets = new Assets() {
+                LargeImageKey = "logo",
+                LargeImageText = "Teardown Multiplayer"
+            },
+        });
 
         Presences.Add(EDiscordState.Connected, new RichPresence {
-            State = $"Playing {Server.Name} with {Server.Clients} people",
+            Details = $"Playing {Server.Gamemode} on {Server.Map}",
+            State = $"Playing with {Server.Clients} others",
+            Party = new Party() {
+                ID = Secrets.CreateFriendlySecret(rand),
+                Privacy = Party.PrivacySetting.Public
+            },
+            Secrets = new Secrets() {
+                JoinSecret = Secrets.CreateFriendlySecret(rand),
+            },
+            Assets = new Assets() {
+                LargeImageKey = "logo",
+                LargeImageText = "Teardown Multiplayer",
+            },
             Timestamps = new Timestamps() {
-            Start = DateTime.UtcNow
-        }});
+                Start = DateTime.UtcNow,
+            }
+        });
 
         Presences.Add(EDiscordState.Connecting, new RichPresence {
-            State = "Connecting to (server_name)",
+            State = "Connecting to Sandbox Server",
             Timestamps = new Timestamps() {
-            Start = DateTime.UtcNow
-        }});
+                Start = DateTime.UtcNow
+            },
+            Assets = new Assets() {
+                LargeImageKey = "logo",
+                LargeImageText = "Teardown Multiplayer"
+            },
+        });
 
-        Client = new DiscordRpcClient("929980695663747083", pipe: -1);
+        Client = new DiscordRpcClient("929980695663747083", pipe: 0);
 
-        Client.OnReady += (sender, msg) => {
-            Console.WriteLine("Connected to discord with user {0}", msg.User.Username);
+        Client.OnJoin += (sender, e) => {
+            Log.General("Join: " + e.Secret);
         };
+
+        Client.OnJoinRequested += (sender, e) => {
+            Log.General("Join requested: " + e.User.ID);
+        };
+
+        Client.Subscribe(EventType.Join | EventType.JoinRequest);
+        Client.RegisterUriScheme();
 
         if (!Client.Initialize()) {
             Console.WriteLine("Discord RPC failed to initialize");
