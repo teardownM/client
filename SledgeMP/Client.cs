ff00fe8a73ec7243a00ccd9b956ccb5cc7a7005d
Dan <31520916+vulcan-dev@users.noreply.github.com>
2022-01-10 03:12:54 +0000

    Changed LoadVox("player2.vox") to "player.vox"
    We need to use the same model but I used the one GGPro made so I had to change the name of yours.

--- SledgeMP/Client.cs
+++ SledgeMP/Client.cs
@@ -199,7 +199,7 @@ public static class Client {
     }
 
     public static void SpawnPlayer(string clientID) {
-        Shape.LoadVox(m_Clients[clientID].Model.sBody, "Assets/Models/player2.vox", "", 1.0f);
+        Shape.LoadVox(m_Clients[clientID].Model.sBody, "Assets/Models/player.vox", "", 1.0f);
         Body.SetTransform(m_Clients[clientID].Model.Body, new Transform(new Vector3(50, 10, 10), new Quaternion(0, 0, 0, 1)));
         m_Clients[clientID].Spawned = true;
 
