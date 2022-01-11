ec9bf10c821fa37d313a33a43a2d404c8114a838
vulcan-dev <vitexuhd@gmail.com>
2022-01-11 20:00:14 +0000

    Changed camera rotation

--- SledgeMP/Client.cs
+++ SledgeMP/Client.cs
@@ -50,15 +50,11 @@ public static class Client {
         if (m_Socket == null || m_Session == null || Server.MatchID == null)
             return;
 
-        Vector2 playerInput = Player.GetPlayerMovementInput();
-        Transform playerTransform = Player.GetCameraTransform();
         Vector3 playerPos = Player.GetPosition();
-        Quaternion playerRotation = Player.GetPlayerCameraTransform().Rotation;
+        Quaternion camRot = Player.GetCameraTransform().Rotation;
 
         var posData = playerPos.X.ToString() + "," + playerPos.Y.ToString() + "," + playerPos.Z.ToString()
-            + "," + playerRotation.X.ToString() + "," + playerRotation.Y.ToString() + "," + playerRotation.Z.ToString() + "," + playerRotation.W.ToString();
-
-        //Log.General("{0} {1} {2} {3} {4} {5} {6}", playerTransform.Position.X, playerTransform.Position.Y, playerTransform.Position.Z, playerRotation.X, playerRotation.Y, playerRotation.Z, playerRotation.W);
+            + "," + camRot.X.ToString() + "," + camRot.Y.ToString() + "," + camRot.Z.ToString() + "," + camRot.W.ToString();
 
         // Every local game tick, send client's position data to Nakama
         m_Socket.SendMatchStateAsync(Server.MatchID, (long)Match.OPCODE.PLAYER_MOVE, posData);
