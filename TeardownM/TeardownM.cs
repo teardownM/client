0179c3f847ecd8fe7856841c79ccdfbdfc1ab452
vulcan-dev <vitexuhd@gmail.com>
2022-04-22 23:09:50 +0100

    Changed filename, fixed formatting and changed author

---
+++ TeardownM/TeardownM.cs
@@ -0,0 +1,18 @@
+// Documentation: https://github.com/44lr/sledge/wiki/API-Documentation
+
+using SledgeLib;
+
+public class TeardownM : ISledgeMod {
+    public string GetName() { return "TeardownM"; }
+    public string GetDescription() { return ""; }
+    public string GetVersion() { return "0.0.1"; }
+    public string GetAuthor() { return "github.com/teardownM"; }
+
+    public void Load() {
+
+    }
+
+    public void Unload() {
+
+    }
+}
