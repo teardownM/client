4f536aa28d012a9784e3f14913242178a9306264
vulcan-dev <vitexuhd@gmail.com>
2022-04-23 01:14:04 +0100

    Added Discord Rich Presence

---
+++ TeardownM/Discord.cs
@@ -0,0 +1,53 @@
+using SledgeLib;
+using DiscordRPC;
+
+public static class Discord {
+    public enum EDiscordState : uint {
+        MainMenu,
+        Connected,
+        Connecting
+    }
+
+    private static DiscordRpcClient? Client;
+    private static Dictionary<EDiscordState, RichPresence> Presences = new Dictionary<EDiscordState, RichPresence>();
+
+    public static bool Initialize() {
+        Presences.Add(EDiscordState.MainMenu, new RichPresence {
+            State = "In Main-Menu",
+            Timestamps = new Timestamps() {
+                Start = DateTime.UtcNow
+            },
+            Assets = new Assets() {
+                LargeImageKey = "logo",
+                LargeImageText = "Teardown Multiplayer"
+            },
+        });
+
+        Client = new DiscordRpcClient("929980695663747083", 0);
+        Client.RegisterUriScheme();
+
+        if (!Client.Initialize()) {
+            Log.Error("Failed to initialize Discord client");
+            return false;
+        }
+
+        Log.Verbose("Discord client initialized");
+
+        return true;
+    }
+
+    public static void Update() {
+        if (Client != null)
+            Client.Invoke();
+    }
+
+    public static void Shutdown() {
+        if (Client != null)
+            Client.Dispose();
+    }
+
+    public static void SetPresence(EDiscordState state) {
+        if (Client != null)
+            Client.SetPresence(Presences[state]);
+    }
+}
--- TeardownM/TeardownM.cs
+++ TeardownM/TeardownM.cs
@@ -9,10 +9,25 @@ public class TeardownM : ISledgeMod {
     public string GetAuthor() { return "github.com/teardownM"; }
 
     public void Load() {
+        if (!Discord.Initialize()) {
+            Log.Error("Failed to initialize Discord client");
+            return;
+        }
 
+        Discord.SetPresence(Discord.EDiscordState.MainMenu);
     }
 
     public void Unload() {
+        Discord.Shutdown();
+    }
 
+    [Callback(ECallbackType.StateChange)]
+    public static void OnStateChange(EGameState GameState) {
+        Log.Verbose("Changed state to " + GameState.ToString());
+    }
+
+    [Callback(ECallbackType.PostUpdate)]
+    public static void OnPostUpdate() {
+        Discord.Update();
     }
 }
