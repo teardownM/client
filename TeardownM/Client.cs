765147a5a5a1fd8ce6afdada082d8163d08cdef4
vulcan-dev <vitexuhd@gmail.com>
2022-04-24 19:18:27 +0100

    Made m_Session nullable

--- TeardownM/Client.cs
+++ TeardownM/Client.cs
@@ -3,7 +3,7 @@ using Nakama;
 public static class Client {
     public static string m_DeviceID = "";
 
-    public static ISession m_Session = null;
+    public static ISession? m_Session = null;
     public static IClient? m_Connection = null;
     public static ISocket? m_Socket = null;
     public static bool m_Connected = false;
