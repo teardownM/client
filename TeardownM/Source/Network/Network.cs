9248333b7dc452556e3a5636c53a4acbb60dc841
vulcan-dev <vitexuhd@gmail.com>
2022-05-06 18:55:45 +0100

    Added m_sMap, I think that's all I done?

--- TeardownM/Source/Network/Network.cs
+++ TeardownM/Source/Network/Network.cs
@@ -3,16 +3,20 @@ using Nakama;
 using Newtonsoft.Json;
 
 public static class Network {
-    private static string m_sAddress = "";
-    private static int m_iPort = 0;
+    public static string m_sAddress = "";
+    public static int m_iPort = 0;
+    public static string m_sMap = "";
+    public static int m_ClientCount = 0;
 
     public static async Task<ISession> Connect(string sAddress, int iPort) {
+        // Disconnect if we're already connected
         if (Server.MatchID != null)
             Disconnect();
 
         m_sAddress = sAddress;
         m_iPort = iPort;
 
+        // Connect to Nakama server
         Client.m_Connection = new Nakama.Client("http", sAddress, iPort, "defaultkey");
         Client.m_Connection.Timeout = 1;
 
@@ -20,8 +24,6 @@ public static class Network {
             Log.Error("Failed to create connection");
             return await Task.FromResult<ISession>(null!);
         }
-
-        Log.Verbose("Device ID: {0}", Client.m_DeviceID);
 
         try {
             Client.m_Session = await Client.m_Connection.AuthenticateDeviceAsync(Client.m_DeviceID, Client.m_DeviceID);
@@ -32,34 +34,40 @@ public static class Network {
             return await Task.FromResult<ISession>(null!);
         }
 
+        // Connect to the socket
         Client.m_Socket = Socket.From(Client.m_Connection);
         await Client.m_Socket.ConnectAsync(Client.m_Session);
 
-        // Client.m_Socket.ReceivedMatchState += OnMatchState;
-        // Client.m_Socket.ReceivedMatchPresence += OnMatchPresence;
+        Client.m_Socket.ReceivedMatchState += MatchState.OnMatchState;
+        Client.m_Socket.ReceivedMatchPresence += MatchState.OnMatchPresence;
 
         IApiRpc response = await Client.m_Connection.RpcAsync(Client.m_Session, "rpc_get_matches");
         JsonConvert.DeserializeObject<Server>(response.Payload.Substring(1, response.Payload.Length - 2));
 
         IMatch match = await Client.m_Socket.JoinMatchAsync(Server.MatchID);
-        Log.Verbose("Successfully connected to match {0}", Server.MatchID!);
-        Client.m_Connected = true;
 
         return Client.m_Session;
     }
 
     public static async void Disconnect() {
-        if (Client.m_Socket == null)
+        // Don't disconnect if we're not connected
+        if (Client.m_Socket == null || !Client.m_Connected)
             return;
 
+        // Leave the match
         if (Server.MatchID != null) {
             await Client.m_Socket.LeaveMatchAsync(Server.MatchID);
             Log.Verbose("Successfully left match {0}", Server.MatchID!);
         }
 
         Log.General("Disconnected from server");
+        Client.m_Connected = false;
 
         Discord.SetPresence(Discord.EDiscordState.MainMenu);
+
+        m_iPort = 0;
+        m_sAddress = "";
+        m_sMap = "";
 
         await Client.m_Socket.CloseAsync();
     }
