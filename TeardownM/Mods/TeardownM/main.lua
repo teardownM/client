dfbe0bc8dc02e7afeb5d1bdcca7937c9299731c1
vulcan-dev <vitexuhd@gmail.com>
2022-05-06 18:54:10 +0100

    Modified main.lua, added template callbacks

--- TeardownM/Mods/TeardownM/main.lua
+++ TeardownM/Mods/TeardownM/main.lua
@@ -1,65 +1,133 @@
+local bDrawMouse = false
+
 function tick()
-    -- 159.89.18.92:7350
-    if InputPressed('l') then
-        LogGeneral('Connecting to server...')
-        TDM_ConnectToServer("127.0.0.1", 7350)
-	elseif InputPressed('k') then
-		LogGeneral('Disconnecting from server...')
-		TDM_DisconnectFromServer()
-	elseif InputPressed('j') then
-		LogGeneral('Getting players...')
-    end
+	if InputPressed('f1') then
+		bDrawMouse = not bDrawMouse
+	end
 end
 
-local InfoBoxSize = {
-	240, 210
-}
-
---------------- This is for debugging -----------------
--- The game crashes often so for now I'm overriding them
-function TDM_GetServerName()
-    return "Server Name"
+-----------------------------------------
+------------- UI Functions --------------
+-----------------------------------------
+function AddToast(eType, sMessage, iDuration)
+	-- This function will add a toast to the screen (a toast is a small message that appears on the screen for a certain amount of time)
 end
 
-function TDM_GetServerMap()
-    return "Awesome Map"
+-----------------------------------------
+--------------- Callbacks ---------------
+-----------------------------------------
+function OnConnecting()
+	-- TDM_SetRichPresence(ERichPresence.Connecting)
 end
 
-function TDM_GetPlayerCount()
-    return 1
+function OnConnect()
+	-- TDM_SetRichPresence(ERichPresence.Connected)
 end
--------------------------------------------------------
+
+function OnDisconnect()
+	-- TDM_SetRichPresence(ERichPresence.MainMenu)
+end
+
+function OnSpawn(iClientID)
+
+end
+
+function OnDeath(iClientID, iKillerID, sWeapon)
+
+end
+
+
+function OnMessage(iClientID, sMessage)
+
+end
+
+function OnShoot(iClientID, sWeapon, iPosX, iPosY)
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- local InfoBoxSize = {
+-- 	240, 210
+-- }
 
 function draw()
-	UiPush()
-		UiFont("regular.ttf", 22)
+	if bDrawMouse then
+		UiMakeInteractive()
+	end
 
-		UiTranslate(10, UiHeight() - InfoBoxSize[2] - 10)
+	-- UiPush()
+	-- 	UiFont("regular.ttf", 22)
 
-		UiPush()
-			UiColor(0, 0, 0, 0.55)
-			UiImageBox('ui/hud/infobox.png', InfoBoxSize[1], InfoBoxSize[2], 6, 6)
-			UiColor(1, 1, 1, 1)
-			UiTranslate(10, UiFontHeight())
+	-- 	UiTranslate(10, UiHeight() - InfoBoxSize[2] - 10)
 
-			UiText('-- Keybindings --')
-			UiTranslate(0, UiFontHeight())
-			UiText('Join Server: L')
-			UiTranslate(0, UiFontHeight())
-			UiText('Disconnect: K')
-			UiTranslate(0, UiFontHeight())
-			UiText('List Players: J')
+	-- 	UiPush()
+	-- 		UiColor(0, 0, 0, 0.55)
+	-- 		UiImageBox('ui/hud/infobox.png', InfoBoxSize[1], InfoBoxSize[2], 6, 6)
+	-- 		UiColor(1, 1, 1, 1)
+	-- 		UiTranslate(10, UiFontHeight())
 
-			UiTranslate(0, UiFontHeight())
-			UiTranslate(0, UiFontHeight())
-			UiText('-- Server Info --')
+	-- 		UiText('-- Keybindings --')
+	-- 		UiTranslate(0, UiFontHeight())
+	-- 		UiText('Join Server: L')
+	-- 		UiTranslate(0, UiFontHeight())
+	-- 		UiText('Disconnect: K')
+	-- 		UiTranslate(0, UiFontHeight())
+	-- 		UiText('List Players: J')
 
-			UiTranslate(0, UiFontHeight())
-			UiText('Server Name: ' .. TDM_GetServerName())
-			UiTranslate(0, UiFontHeight())
-			UiText('Server Map: ' .. TDM_GetServerMap())
-			UiTranslate(0, UiFontHeight())
-			UiText('Player Count: ' .. TDM_GetPlayerCount())
-		UiPop()
-    UiPop()
+	-- 		UiTranslate(0, UiFontHeight())
+	-- 		UiTranslate(0, UiFontHeight())
+	-- 		UiText('-- Server Info --')
+
+	-- 		UiTranslate(0, UiFontHeight())
+	-- 		UiText('Server Name: ' .. TDM_GetServerName())
+	-- 		UiTranslate(0, UiFontHeight())
+	-- 		UiText('Server Map: ' .. TDM_GetServerMap())
+	-- 		UiTranslate(0, UiFontHeight())
+	-- 		UiText('Player Count: ' .. TDM_GetPlayerCount())
+	-- 	UiPop()
+    -- UiPop()
+end
+
+function OnConnecting(serverInfo)
+
+end
+
+function OnConnect(serverInfo)
+	
 end
