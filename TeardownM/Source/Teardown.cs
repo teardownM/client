ad2747e7214f3b4c9bf100c0d8c019b4bb407c6d
vulcan-dev <vitexuhd@gmail.com>
2022-05-07 02:25:56 +0100

    Cleaned up some code

--- TeardownM/Source/Teardown.cs
+++ TeardownM/Source/Teardown.cs
@@ -6,7 +6,7 @@ public static class Teardown {
     public static IntPtr pGameBase;
     public static IntPtr pHwnd;
 
-    public static Dictionary<string, int> Offsets = new Dictionary<string, int>() {
+    public static readonly Dictionary<string, int> Offsets = new Dictionary<string, int>() {
         { "UiReload", 0xAD98 },
         { "DebugJZ1", 0x3C8AD },
         { "DebugJZ2", 0xC716 },
@@ -17,13 +17,13 @@ public static class Teardown {
     public static string sGameVersion = "";
 
     public static void EnableDebugMenu() {
-        Memory.Write(Teardown.pGameBase + Teardown.Offsets["DebugJZ1"], new byte[] { 0x90, 0x90 });
-        Memory.Write(Teardown.pGameBase + Teardown.Offsets["DebugJZ2"], new byte[] { 0x90, 0x90 });
+        Memory.Write(pGameBase + Offsets["DebugJZ1"], new byte[] { 0x90, 0x90 });
+        Memory.Write(pGameBase + Offsets["DebugJZ2"], new byte[] { 0x90, 0x90 });
     }
 
     public static void DisableDebugMenu() {
-        Memory.Write(Teardown.pGameBase + Teardown.Offsets["DebugJZ1"], new byte[] { 0x74, 0x24 });
-        Memory.Write(Teardown.pGameBase + Teardown.Offsets["DebugJZ2"], new byte[] { 0x74, 0x18 });
+        Memory.Write(pGameBase + Offsets["DebugJZ1"], new byte[] { 0x74, 0x24 });
+        Memory.Write(pGameBase + Offsets["DebugJZ2"], new byte[] { 0x74, 0x18 });
     }
 
     public static bool Initialize() {
@@ -33,8 +33,8 @@ public static class Teardown {
         Thread tCheckOffsets = new Thread(() => {
             Thread.Sleep(1000); // Wait for game to load
 
-            foreach (KeyValuePair<string, int> iOffset in Teardown.Offsets) {
-                byte[] bData = Memory.Read(Teardown.pGameBase + iOffset.Value, 2);
+            foreach (KeyValuePair<string, int> iOffset in Offsets) {
+                byte[] bData = Memory.Read(pGameBase + iOffset.Value, 2);
                 Log.General("Offset {0} = {1:X}", iOffset.Key, BitConverter.ToUInt16(bData, 0));
                 if (bData.Length == 0) {
                     Log.Error("Failed reading offset {0}. If you experience any issues, please be patient and wait for an update.", iOffset.Key);
@@ -53,22 +53,22 @@ public static class Teardown {
 
     public static void GetProcessInfo() {
         Process pTeardown = Process.GetCurrentProcess();
-        Teardown.sGamePath = Directory.GetCurrentDirectory();
-        Teardown.pProcessHandle = pTeardown.Handle;
-        Teardown.iProcessID = pTeardown.Id;
-        Teardown.pGameBase = pTeardown.MainModule!.BaseAddress;
-        Teardown.pHwnd = pTeardown.MainWindowHandle;
+        sGamePath = Directory.GetCurrentDirectory();
+        pProcessHandle = pTeardown.Handle;
+        iProcessID = pTeardown.Id;
+        pGameBase = pTeardown.MainModule!.BaseAddress;
+        pHwnd = pTeardown.MainWindowHandle;
     }
 
     public static string GetGameVersion() {
-        if (Teardown.sGameVersion != "")
-            return Teardown.sGameVersion;
+        if (sGameVersion != "")
+            return sGameVersion;
 
-        byte[] bVersion = Memory.Read(Teardown.pGameBase + Teardown.Offsets["Version"], 5);
-        Teardown.sGameVersion = "";
+        byte[] bVersion = Memory.Read(pGameBase + Offsets["Version"], 5);
+        sGameVersion = "";
         for (int i = 0; i < bVersion.Length; i++)
-            Teardown.sGameVersion += (char)bVersion[i];
+            sGameVersion += (char)bVersion[i];
 
-        return Teardown.sGameVersion;
+        return sGameVersion;
     }
 }
