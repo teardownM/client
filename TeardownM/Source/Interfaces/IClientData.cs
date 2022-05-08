9708e690e3dd3a121811baa1faa3c0f96f8ec54b
vulcan-dev <vitexuhd@gmail.com>
2022-05-08 22:24:48 +0100

    Removed Newline from `IClientData.cs`

--- TeardownM/Source/Interfaces/IClientData.cs
+++ TeardownM/Source/Interfaces/IClientData.cs
@@ -3,7 +3,6 @@ namespace TeardownM.Interfaces;
 public interface IClientData {
     public string Name { get; set; }
     public string IP { get; set; }
-
     public long SteamID { get; set; }
     public string SteamName { get; set; }
     public string SteamAvatar { get; set; }
