d95408211e2de777e4f671d28454c63ab56cac74
vulcan-dev <vitexuhd@gmail.com>
2022-05-08 21:28:22 +0100

    Cleanup lua mod

--- TeardownM/Mods/TeardownM/main.lua
+++ TeardownM/Mods/TeardownM/main.lua
@@ -6,11 +6,15 @@ function tick()
 	end
 end
 
+function draw()
+	if bDrawMouse then UiMakeInteractive() end
+end
+
 -----------------------------------------
 ------------- UI Functions --------------
 -----------------------------------------
 function AddToast(eType, sMessage, iDuration)
-	-- This function will add a toast to the screen (a toast is a small message that appears on the screen for a certain amount of time)
+	--[[ Adds a message to the screen, called from C# ]]--
 end
 
 -----------------------------------------
@@ -36,98 +40,10 @@ function OnDeath(iClientID, iKillerID, sWeapon)
 
 end
 
-
 function OnMessage(iClientID, sMessage)
 
 end
 
 function OnShoot(iClientID, sWeapon, iPosX, iPosY)
 
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- local InfoBoxSize = {
--- 	240, 210
--- }
-
-function draw()
-	if bDrawMouse then
-		UiMakeInteractive()
-	end
-
-	-- UiPush()
-	-- 	UiFont("regular.ttf", 22)
-
-	-- 	UiTranslate(10, UiHeight() - InfoBoxSize[2] - 10)
-
-	-- 	UiPush()
-	-- 		UiColor(0, 0, 0, 0.55)
-	-- 		UiImageBox('ui/hud/infobox.png', InfoBoxSize[1], InfoBoxSize[2], 6, 6)
-	-- 		UiColor(1, 1, 1, 1)
-	-- 		UiTranslate(10, UiFontHeight())
-
-	-- 		UiText('-- Keybindings --')
-	-- 		UiTranslate(0, UiFontHeight())
-	-- 		UiText('Join Server: L')
-	-- 		UiTranslate(0, UiFontHeight())
-	-- 		UiText('Disconnect: K')
-	-- 		UiTranslate(0, UiFontHeight())
-	-- 		UiText('List Players: J')
-
-	-- 		UiTranslate(0, UiFontHeight())
-	-- 		UiTranslate(0, UiFontHeight())
-	-- 		UiText('-- Server Info --')
-
-	-- 		UiTranslate(0, UiFontHeight())
-	-- 		UiText('Server Name: ' .. TDM_GetServerName())
-	-- 		UiTranslate(0, UiFontHeight())
-	-- 		UiText('Server Map: ' .. TDM_GetServerMap())
-	-- 		UiTranslate(0, UiFontHeight())
-	-- 		UiText('Player Count: ' .. TDM_GetPlayerCount())
-	-- 	UiPop()
-    -- UiPop()
-end
-
-function OnConnecting(serverInfo)
-
-end
-
-function OnConnect(serverInfo)
-	
 end
