08b49e21bbe493a553f420cabb3bfd6d57226940
vulcan-dev <vitexuhd@gmail.com>
2022-05-08 21:33:36 +0100

    Added NetSocket.cs

---
+++ TeardownM/Source/Network/NetSocket.cs
@@ -0,0 +1,35 @@
+﻿using Nakama;
+
+namespace TeardownM.Network;
+
+public static class NetSocket {
+    public static async Task<ISocket> CreateSocket(IClient Connection, ISession? Session) {
+        ISocket? socket;
+        
+        socket = Socket.From(Connection);
+        await socket.ConnectAsync(Session);
+        
+        socket.ReceivedError += ReceivedError;
+        socket.Connected += Connected;
+        socket.Closed += Closed;
+        
+        socket.ReceivedMatchState += MatchState.OnMatchState;
+        socket.ReceivedMatchPresence += MatchState.OnMatchPresence;
+
+        Log.Verbose("Socket created");
+        
+        return socket;
+    }
+    
+    public static void ReceivedError(Exception e) {
+        Log.Error("NetSocket ReceivedError: " + e.Message);
+    }
+
+    public static void Connected() {
+        Log.Verbose("NetSocket Connected");
+    }
+
+    public static void Closed() {
+        Log.Verbose("NetSocket Closed");
+    }
+}
