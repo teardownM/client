c7b82c40033f956ae0f4b6392b406c7f4c2cd4bc
vulcan-dev <vitexuhd@gmail.com>
2022-05-09 03:23:22 +0100

    Removed IClientData

--- TeardownM/Source/Interfaces/IClientData.cs
+++
@@ -1,9 +0,0 @@
-namespace TeardownM.Interfaces;
-
-public interface IClientData {
-    public string Name { get; set; }
-    public string IP { get; set; }
-    public long SteamID { get; set; }
-    public string SteamName { get; set; }
-    public string SteamAvatar { get; set; }
-}
